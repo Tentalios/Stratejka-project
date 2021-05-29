b2a2dda028a63d6e7c993e085edfe80724d0fe58
SawUshKA2001 <83693855+SawUshKA2001@users.noreply.github.com>
2021-05-29 14:40:25 +0500

    Второе сохранение на гитхаб.
    Молимся богу, чтобы ничего не сломалось.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -4,6 +4,12 @@ var context = canvas.getContext("2d");
 var leftPlayerMenu = $("section.left_player");
 var rightPlayerMenu = $("section.right_player");
 
+var leftPlayerHealthRender = $("section.left_player section.info p.health");
+var rightPlayerHealthRender = $("section.right_player section.info p.health");
+
+var leftPlayerMoneyRender = $("section.left_player section.info p.money");
+var rightPlayerMoneyRender = $("section.right_player section.info p.money");
+
 var endOfTurn = $("input.end_turn");
 
 var units = $("section.unit");
@@ -18,9 +24,18 @@ canvas.height = 12*cellHeight;	// Задание высоты поля.
 
 var cellArray = [];	// Двумерный массив тайлов.
 
-var playerTurn = 0;	// Переменная содержащая информацию о том, чей ход.
+var playerTurn = "left";	// Переменная содержащая информацию о том, чей ход.
 
 var selectedUnitMenu = "";	// Имя выбранного в меню покупки юнита 
+
+var leftPlayerUnitsArray = [];	// Список юнитов левого игрока
+var rightPlayerUnitsArray = [];	// Список юнитов правого игрока
+
+var leftPlayerMoney = 1000;
+var rightPlayerMoney = 1000;
+
+var leftPlayerHealth = 1000;
+var rightPlayerHealth = 1000;
 
 ///////////
 
@@ -33,6 +48,30 @@ cellGrass.src = "assets/images/tiles/grass.png";
 
 ////
 
+//// Юниты
+
+// Изображения юнитов
+
+var leftRat = new Image();
+leftRat.src="assets/images/units/rat_left.jpg";
+var rightRat = new Image();
+rightRat.src="assets/images/units/rat_right.jpg";
+
+//
+
+var unitsArray = {
+	rat:{
+		health: 100,
+		damage: 20,
+		speed: 2,
+		range: 2,
+		price: 50,
+		leftImage: leftRat,
+		rightImage: rightRat
+	}
+};
+
+////
 
 ///////////
 
@@ -56,14 +95,15 @@ for (var cellX = 0; cellX<canvas.width/cellWidth; cellX++) {
 
 function takeTurn(player){
 	units.removeClass("select_unit");
+	selectedUnitMenu = "";
 	switch(player){
 		case "leftTurnEnd":
-			playerTurn = 1;
+			playerTurn = "right";
 			leftPlayerMenu.addClass("non-display");
 			rightPlayerMenu.removeClass("non-display");
 			break;
 		case "rightTurnEnd":
-			playerTurn = 0;
+			playerTurn = "left";
 			rightPlayerMenu.addClass("non-display");
 			leftPlayerMenu.removeClass("non-display");
 			break;
@@ -88,7 +128,79 @@ function selectUnitMenu(unitName){
 
 ////
 
-/////////// Игра
+//// Обработка нажатия по полю
+
+function getCursorPosition(canvas, event) {
+    const rect = canvas.getBoundingClientRect()
+    cursorX = Math.floor((event.clientX - rect.left)/cellWidth)*cellWidth;
+    cursorY = Math.floor((event.clientY - rect.top)/cellHeight)*cellHeight;
+    console.log('x'+cursorX+'---y'+cursorY);
+    if(event.which==3) // правый клик
+	{
+			
+	}
+	if(event.which==1) // левый клик
+	{ 
+		// Высадка юнитов на поле
+
+			if(selectedUnitMenu != ""){
+				switch(playerTurn){
+					case "right":
+						if(rightPlayerMoney>=unitsArray[selectedUnitMenu].price){
+							rightPlayerUnitsArray.push({
+								x: cursorX,
+								y: cursorY,
+								cellX: cursorX/cellWidth,
+								cellY: cursorY/cellHeight,
+								type: selectedUnitMenu,
+								maxHealth: unitsArray[selectedUnitMenu].health,
+								currentHealth: unitsArray[selectedUnitMenu].health,
+								damage: unitsArray[selectedUnitMenu].damage,
+								speed: unitsArray[selectedUnitMenu].speed,
+								range: unitsArray[selectedUnitMenu].range,
+								img: unitsArray[selectedUnitMenu].rightImage,
+								turn: 0
+							});
+							rightPlayerMoney-=unitsArray[selectedUnitMenu].price;
+						}
+						break;
+					case "left":
+						if(leftPlayerMoney>=unitsArray[selectedUnitMenu].price){
+							leftPlayerUnitsArray.push({
+								x: cursorX,
+								y: cursorY,
+								cellX: cursorX/cellWidth,
+								cellY: cursorY/cellHeight,
+								type: selectedUnitMenu,
+								maxHealth: unitsArray[selectedUnitMenu].health,
+								currentHealth: unitsArray[selectedUnitMenu].health,
+								damage: unitsArray[selectedUnitMenu].damage,
+								speed: unitsArray[selectedUnitMenu].speed,
+								range: unitsArray[selectedUnitMenu].range,
+								img: unitsArray[selectedUnitMenu].leftImage,
+								turn: 0
+							});
+							leftPlayerMoney-=unitsArray[selectedUnitMenu].price;
+						}
+						break;
+				}
+			}
+
+		//
+	} 
+}
+
+////
+
+//// Нажатие по полю
+
+canvas.addEventListener('mousedown', function(e) {
+    getCursorPosition(canvas, e)
+});
+
+////
+
+/////// Игра
 
 function game(){
 	// Отрисовка тайлов травы
@@ -98,10 +210,27 @@ function game(){
 		});
 	});
 	// 
-
+	// Отрисовка юнитов на поле
+	if(leftPlayerUnitsArray.length > 0){
+		leftPlayerUnitsArray.forEach(function(item){
+			context.drawImage(item.img, item.x, item.y, cellWidth, cellHeight);
+		});
+	}
+	if(rightPlayerUnitsArray.length > 0){
+		rightPlayerUnitsArray.forEach(function(item){
+			context.drawImage(item.img, item.x, item.y, cellWidth, cellHeight);
+		});
+	}
+	//
+	// Отрисовка денег и здоровья игроков
+	leftPlayerHealthRender.text(leftPlayerHealth);
+	rightPlayerHealthRender.text(rightPlayerHealth);
+	leftPlayerMoneyRender.text(leftPlayerMoney);
+	rightPlayerMoneyRender.text(rightPlayerMoney);
+	//
 	requestAnimationFrame(game);
 }
 
-///////////
+///////
 
 ///////////
